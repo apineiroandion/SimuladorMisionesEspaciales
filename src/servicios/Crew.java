73586f6fcadb576032d186c43ef4f221eb86ab65
Angel <apineiroandion@danielcastelao.org>
2024-04-17 12:14:48 +0200

    methods to serialize and deserialize crew and ships

--- src/servicios/Crew.java
+++ src/servicios/Crew.java
@@ -1,9 +1,36 @@
 package servicios;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Crew {
     ArrayList<Crew> crew = new ArrayList<>();
+    public void serializeCrewMemberList(Crew crew) {
+        try {
+            FileOutputStream fos = new FileOutputStream("crew.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(crew);
+            oos.close();
+            fos.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public Crew deserializeCrewMemberList(Crew crew) {
+        try {
+            FileInputStream fis = new FileInputStream("crew.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            crew = (Crew) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return crew;
+    }
 
     public Crew() {
     }
--- src/servicios/Ships.java
+++ src/servicios/Ships.java
@@ -1,10 +1,38 @@
 package servicios;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class Ships {
     ArrayList<Ship> ships = new ArrayList<>();
     public Ships() {}
+
+    public void serializeShipList(Ships ships) {
+        try {
+            FileOutputStream fos = new FileOutputStream("ships.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(ships);
+            oos.close();
+            fos.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public Ships deserializeShipList(Ships ships) {
+        try {
+            FileInputStream fis = new FileInputStream("ships.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ships = (Ships) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return ships;
+    }
 
     public ArrayList<Ship> getShips() {
         return ships;
