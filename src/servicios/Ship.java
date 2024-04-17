080765026cd0904be4fdafd4eae6c0a9ea457fe9
Angel <apineiroandion@danielcastelao.org>
2024-04-17 11:28:01 +0200

    add inUse boolean

--- src/servicios/Ship.java
+++ src/servicios/Ship.java
@@ -4,6 +4,7 @@ public class Ship {
     private int id;
     private String shipName;
     private String shipType;
+    private boolean inUse;
 
     public Ship(int id, String shipName, String shipType) {
         this.id = id;
@@ -33,5 +34,13 @@ public class Ship {
 
     public void setShipType(String shipType) {
         this.shipType = shipType;
+    }
+
+    public boolean isInUse() {
+        return inUse;
+    }
+
+    public void setInUse(boolean inUse) {
+        this.inUse = inUse;
     }
 }
