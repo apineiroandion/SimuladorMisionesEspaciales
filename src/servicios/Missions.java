3d54ff7ce6777ba3b6a592cefa63422770af96d3
Angel <apineiroandion@danielcastelao.org>
2024-04-17 12:07:40 +0200

    methods to serialize and deserialize a list of missions

--- src/servicios/Missions.java
+++ src/servicios/Missions.java
@@ -1,10 +1,35 @@
 package servicios;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Missions {
     ArrayList<Mission> missions = new ArrayList<>();
     public Missions() {}
+
+    public void serializeMissions (Missions missionList){
+        try {
+            FileOutputStream fos = new FileOutputStream("Missions.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(missionList);
+            oos.close();
+            fos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public Missions deserializeMissions(Missions missionList){
+        try {
+            FileInputStream fis = new FileInputStream("Missions.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            missionList = (Missions) ois.readObject();
+            ois.close();
+            fis.close();
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return missionList;
+    }
 
     public ArrayList<Mission> getMissions() {
         return missions;
@@ -13,4 +38,5 @@ public class Missions {
     public void setMissions(ArrayList<Mission> missions) {
         this.missions = missions;
     }
+
 }
