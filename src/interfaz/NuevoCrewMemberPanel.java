687572cde3c5d949596ca4812b1b286311c22a75
angel <apineiroandion@danielcastelao.org>
2024-04-20 12:14:06 +0200

    mejoro interfaz

--- src/interfaz/NuevoCrewMemberPanel.java
+++ src/interfaz/NuevoCrewMemberPanel.java
@@ -16,57 +16,76 @@ public class NuevoCrewMemberPanel extends JPanel {
     private JLabel jobLabel;
     private JComboBox job;
     private String[] jobList = {"Ingeniero", "Piloto", "Fisico", "Storm Trooper"};
+    private JButton newCrewMemberButton;
 
     public NuevoCrewMemberPanel() {
         setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
         newCrewMemberLabel = new JLabel("New Crew Member");
-
-        name = new JTextField(15);
         c.gridx = 0;
         c.gridy = 0;
         add(newCrewMemberLabel, c);
 
-        surnameLabel = new JLabel("Surname");
+        nameLabel = new JLabel("Name");
         c.gridx = 0;
         c.gridy = 1;
+        add(nameLabel, c);
+
+        name = new JTextField(15);
+        c.gridx = 1;
+        c.gridy = 1;
+        add(name, c);
+
+        surnameLabel = new JLabel("Surname");
+        c.gridx = 0;
+        c.gridy = 2;
         add(surnameLabel, c);
 
         surname = new JTextField(15);
         c.gridx = 1;
-        c.gridy = 1;
+        c.gridy = 2;
         add(surname, c);
 
         ageLabel = new JLabel("Age");
         c.gridx = 0;
-        c.gridy = 2;
+        c.gridy = 3;
         add(ageLabel, c);
 
         age = new JTextField(15);
         c.gridx = 1;
-        c.gridy = 2;
+        c.gridy = 3;
         add(age, c);
 
         genderLabel = new JLabel("Gender");
         c.gridx = 0;
-        c.gridy = 3;
+        c.gridy = 4;
         add(genderLabel, c);
 
         gender = new JTextField(15);
         c.gridx = 1;
-        c.gridy = 3;
+        c.gridy = 4;
         add(gender, c);
 
         jobLabel = new JLabel("Job");
         c.gridx = 0;
-        c.gridy = 4;
+        c.gridy = 5;
         add(jobLabel, c);
 
         job = new JComboBox(jobList);
         c.gridx = 1;
-        c.gridy = 4;
+        c.gridy = 5;
         add(job, c);
+
+        c.gridx = 0;
+        c.gridy = 6;
+        add(Box.createVerticalStrut(10), c);
+
+        newCrewMemberButton = new JButton("New Crew Member");
+        c.gridx = 0;
+        c.gridy = 7;
+        c.gridwidth = 2;
+        add(newCrewMemberButton, c);
 
 
 
