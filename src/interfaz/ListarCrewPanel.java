764f078604519451dfd8b588d5c018abb913d7d3
angel <apineiroandion@danielcastelao.org>
2024-04-20 13:46:07 +0200

    tareas

---
+++ src/interfaz/ListarCrewPanel.java
@@ -0,0 +1,10 @@
+package interfaz;
+
+import servicios.Crew;
+
+import javax.swing.*;
+
+public class ListarCrewPanel extends JPanel {
+    JTable crewTable = new JTable();
+    //TODO : HACER DATA PARA LA TABLA Y RESTO DE COSAS DE LA TABLA
+}
